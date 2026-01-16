50e848506d4a4385828714907c1b66686b4bff0c
Andrea Jans <andreaw@jans.org>
2026-01-16 07:15:47 -1000

    Refactor database connection logic and enhance test table creation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 
 from dbconnfactory import DBConnFactory
 
+CHAR_BIGINT = ("A" * 244)
+CHAR_UUID = ("A" * 236)
+
 def main() -> None:
     """
     Docstring for main
@@ -11,11 +14,17 @@ def main() -> None:
         print("Database connection established successfully. {}".format(db_conn.dsn))
         with db_conn.cursor() as cursor:
             cursor.execute("SELECT version();")
-            db_version = cursor.fetchone()
+            db_version = cursor.fetchall()
+            if db_version is None or len(db_version) == 0:
+                raise Exception("Failed to retrieve database version.")
+            else:
+                db_version = db_version[0][0]
             print(f"Database version: {db_version}")
-            cursor.execute("COMMIT;")
-            cursor.execute("DROP TABLE IF EXISTS test_bigint;")
-            cursor.execute("CREATE TABLE test_bigint (id BIGSERIAL PRIMARY KEY, data CHAR(248));")
+            print("Creating test tables...")
+            cursor.execute("DROP TABLE IF EXISTS test_bigint; CREATE TABLE test_bigint (id BIGSERIAL PRIMARY KEY, data CHAR(244) NOT NULL);")
+            cursor.execute("DROP TABLE IF EXISTS test_uuidv4; CREATE TABLE test_uuidv4 (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), data CHAR(236) NOT NULL);")
+            cursor.execute("DROP TABLE IF EXISTS test_uuidv7; CREATE TABLE test_uuidv7 (id UUID PRIMARY KEY DEFAULT uuidv7(), data CHAR(236) NOT NULL);")
+            print("Created test tables successfully.")
 
 
 if __name__ == "__main__":
