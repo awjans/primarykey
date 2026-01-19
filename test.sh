bda77e14f25458379ce7533851e215dc5004c1b2
Andrea Jans <andreaw@jans.org>
2026-01-18 15:26:58 -1000

    Fix operations parameter in test script to use a variable instead of a positional argument

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ops=${1:-1000}
+
 ldir=$(realpath ~/logs)
 mdir=$(realpath ~/metrics)
 
@@ -7,7 +9,7 @@ for w in 1 2 4 8 16; do
 	for b in 1 2 10 25 50 100 250 500 1000; do
 		for p in "bigint" "uuidv4" "uuidv7"; do
             echo "Running with workers=$w, batchsize=$b, pktype=$p"
-            /home/andrea/src/primarykey/.venv/bin/python /home/andrea/src/primarykey/src/main.py --logdir $ldir --metricsdir $mdir --operations $1 --workers $w --batchsize $b --pktype $p
+            /home/andrea/src/primarykey/.venv/bin/python /home/andrea/src/primarykey/src/main.py --logdir $ldir --metricsdir $mdir --operations $ops --workers $w --batchsize $b --pktype $p
 		done
 	done
 done
