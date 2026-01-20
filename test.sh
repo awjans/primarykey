7186db4298f3ce2a55a256b770fd8024b98341ad
Andrea Jans <andreaw@jans.org>
2026-01-19 18:27:46 -1000

    Refactor test script and add SLURM job configuration for PostgreSQL testing

--- test.sh
+++ test.sh
@@ -1,15 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "$SCRIPT_DIR" || exit 1
+pwd
 
 ops=${1:-1000}
 
 ldir=$(realpath ~/logs)
 mdir=$(realpath ~/metrics)
 
+source ./.venv/bin/activate
+
 for w in 1 2 4 8 16; do
 	for b in 1 2 10 25 50 100 250 500 1000; do
 		for p in "bigint" "uuidv4" "uuidv7"; do
             echo "Running with workers=$w, batchsize=$b, pktype=$p"
-            /home/andrea/src/primarykey/.venv/bin/python /home/andrea/src/primarykey/src/main.py --logdir $ldir --metricsdir $mdir --operations $ops --workers $w --batchsize $b --pktype $p
+            ./.venv/bin/python ./src/main.py --logdir $ldir --metricsdir $mdir --operations $ops --workers $w --batchsize $b --pktype $p
 		done
 	done
 done
+
+deactivate
